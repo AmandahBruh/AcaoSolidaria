3ade16368a158eddebc140995276f10562047d00
Joao Gabriel <konestiqui@gmail.com>
2023-05-29 16:14:45 -0300

    Primeiros commits Joao G

---
+++ src/components/homeScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react-native";
+import { View } from "react-native";
+import styles from "../utils/style";
+
+export default function Home(){
+    return (
+        <View style={styles.containerH}>
+
+        </View>
+    );
+}
---
+++ src/utils/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    containerH:{
+        flex: 1,
+        backgroundColor:"#fff",
+        alignItems:"center",
+        justifyContent:"center"
+        },
+})
