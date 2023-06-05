a22ce77ac31fa9004a6c074a433423e87d55c7b8
Joao Gabriel <konestiqui@gmail.com>
2023-06-05 16:12:26 -0300

    scrollview vermais

--- src/components/VerMaisScreen.jsx
+++ src/components/VerMaisScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import styles from "../utils/styles";
 import Header from "./header/Header";
 
@@ -6,7 +6,7 @@ import Header from "./header/Header";
 /**
  * @author          João Gabriel <joao.alves1@alunos.sc.senac.br>
  * @description     Pagina responsável por mostrar as instituições em ordem e separada por tipo
- * @version         1.0.0
+ * @version         1.0.2
  * @since           1.0.0
  *
  * @export          {function} vermaisScreen
@@ -15,11 +15,26 @@ import Header from "./header/Header";
 
 export default function VerMaisScreen({}){
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
+                        <Header/>
 
-            <Header/>
+            <ScrollView>
 
             <Text>Aqui ó</Text>
-        </View>
+
+            <View style={styles.lines}>
+                <Image />
+            </View>
+
+            <View style={styles.lines}>
+                <Image />
+            </View>
+
+            <View style={styles.lines}>
+                <Image />
+            </View>
+
+            </ScrollView>
+        </SafeAreaView>
     );
 }
